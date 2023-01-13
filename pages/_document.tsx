fbbf5ba3145010a5e83460119461864aad32a78c
codeisneverodd <codeisneverodd@gmail.com>
2023-01-13 20:38:23 +0900

    fix: darkMode flickering

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,19 @@
-import { Html, Head, Main, NextScript } from "next/document";
-
+import { Html, Head, Main, NextScript } from 'next/document';
+const themeScript = `
+(function () {
+  const isDarkMode =
+      localStorage.theme === 'dark' ||
+      (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches);
+  document.documentElement.classList.toggle('dark', isDarkMode);
+})();
+`;
 export default function Document() {
   return (
-    <Html lang="ko-KR" className="dark">
-      <Head />
-      <body className="bg-lightGray dark:bg-black text-black dark:text-white">
+    <Html lang="ko-KR" className="group/root dark">
+      <Head>
+        <script dangerouslySetInnerHTML={{ __html: themeScript }}></script>
+      </Head>
+      <body className="bg-lightGray text-black dark:bg-black dark:text-white">
         <Main />
         <NextScript />
       </body>
--- pages/components/DarkModeToggleBtn/index.tsx
+++ pages/components/DarkModeToggleBtn/index.tsx
@@ -3,26 +3,15 @@ import { ChangeEventHandler, useLayoutEffect, useRef } from 'react';
 import { tw } from 'utils/tailwindMerge';
 
 export function DarkModeToggleBtn({ children, className, ...restProps }: DarkModeBtnProps<'label'>) {
-  const inputRef = useRef(null);
-
-  useLayoutEffect(() => {
-    const isDarkMode =
-      localStorage.theme === 'dark' ||
-      (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches);
-    document.documentElement.classList.toggle('dark', isDarkMode);
-    (inputRef.current as unknown as HTMLInputElement).checked = isDarkMode;
-  }, []);
-
   const handleDarkModeChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-    const { checked } = e.target;
-    localStorage.theme = checked ? 'dark' : 'light';
+    localStorage.theme = localStorage.theme === 'dark' ? 'light' : 'light';
     document.documentElement.classList.toggle('dark', localStorage.theme === 'dark');
   };
   return (
     <label className={tw('relative inline-block h-[34px] w-[60px]', className)} {...restProps}>
-      <input ref={inputRef} type="checkbox" className="peer h-0 w-0 opacity-0" onChange={handleDarkModeChange} />
-      <span className="absolute inset-0 cursor-pointer rounded-[34px] bg-darkGray transition-all peer-checked:bg-lightGray"></span>
-      <span className="absolute left-[4px] bottom-[4px] h-[26px] w-[26px] cursor-pointer rounded-full border-2 border-darkGray bg-white transition-all peer-checked:translate-x-[26px] peer-checked:bg-white "></span>
+      <input type="checkbox" className="h-0 w-0 opacity-0" onChange={handleDarkModeChange} />
+      <span className="absolute inset-0 cursor-pointer rounded-[34px] bg-darkGray transition-all group-[.dark]/root:bg-lightGray"></span>
+      <span className="absolute left-[4px] bottom-[4px] h-[26px] w-[26px] cursor-pointer rounded-full border-2 border-darkGray bg-white transition-all group-[.dark]/root:translate-x-[26px] group-[.dark]/root:bg-white "></span>
     </label>
   );
 }
